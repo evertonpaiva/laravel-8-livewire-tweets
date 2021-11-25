246f5ed9079206c3913c5e9d87c26b6c5c56a2f6
Everton Paiva <everton.paiva@ufvjm.edu.br>
2021-11-25 17:29:37 -0300

    Adicionando componente livewire para exibir tweets

---
+++ app/Http/Livewire/ShowTweets.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use App\Models\Tweet;
+use Livewire\Component;
+
+class ShowTweets extends Component
+{
+    public $message = 'apenas um teste 2';
+
+    public function render()
+    {
+        $tweets = Tweet::with('user')->get();
+
+        return view('livewire.show-tweets', [
+            'tweets' => $tweets
+        ]);
+    }
+}
