362211e2745f0288df582dff96ba6a402fe2c8d4
Everton Paiva <everton.paiva@ufvjm.edu.br>
2021-11-26 15:14:47 -0300

    Atualizando classes CSS para as classes do Tailwind.

--- app/Http/Livewire/User/UploadPhoto.php
+++ app/Http/Livewire/User/UploadPhoto.php
@@ -2,9 +2,9 @@
 
 namespace App\Http\Livewire\User;
 
-use Illuminate\Support\Str;
 use Livewire\Component;
 use Livewire\WithFileUploads;
+use Illuminate\Support\Str;
 
 class UploadPhoto extends Component
 {
@@ -20,17 +20,16 @@ class UploadPhoto extends Component
     public function storagePhoto()
     {
         $this->validate([
-           'photo' => 'required|image|max:1024'
+            'photo' => 'required|image|max:1024'
         ]);
 
         $user = auth()->user();
 
         $nameFile = Str::slug($user->name) . '.' . $this->photo->getClientOriginalExtension();
 
-        $path = $this->photo->storeAs('users', $nameFile);
-        if($path) {
+        if ($path = $this->photo->storeAs('users', $nameFile)) {
             $user->update([
-                'profile_photo_path' => $path
+                'profile_photo_path' => $path,
             ]);
         }
 
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,37 +1,34 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Fonts -->
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
+    <!-- Fonts -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap">
 
-        <!-- Styles -->
-        <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
-        @livewireStyles
+    @livewireStyles
 
-        <!-- Scripts -->
-        <script src="{{ mix('js/app.js') }}" defer></script>
-    </head>
-    <body class="font-sans antialiased">
-        <x-jet-banner />
+    <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.7.0/dist/alpine.js" defer></script>
+</head>
+<body class="font-sans antialiased bg-gray-100">
+<div class="container mx-auto h-200">
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+    <!-- Page Content -->
+    <main>
+        {{ $slot }}
+    </main>
+</div>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+@stack('modals')
 
-        @stack('modals')
-
-        @livewireScripts
-    </body>
+@livewireScripts
+</body>
 </html>
